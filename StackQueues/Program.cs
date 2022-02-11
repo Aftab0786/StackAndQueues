64efa34fc874d351e3fa429954a3bd246e352eb6
Aftab-1998 <alam41976@gmail.com>
2022-02-11 16:08:57 +0530

    [AFTAB1] Add; Added the node in required format

---
+++ StackQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackQueues;
+class program
+{
+    static void Main(string[] args)
+    {
+        Stack stack = new Stack();
+        stack.Push(70);
+        stack.Push(30);
+        stack.Push(56);
+        stack.Display();
+    }
+}
